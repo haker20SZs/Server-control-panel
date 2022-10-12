b18f59a670eb52a734837fd5c0e4a95f43f321e6
VeBICOoder <79506370+haker20SZs@users.noreply.github.com>
2022-10-12 11:13:45 +0700

    Add files via upload

--- BetrimPanel Linux beta/vendor/signin.php
+++ BetrimPanel Linux beta/vendor/signin.php
@@ -7,7 +7,7 @@
     $password = $_POST['password'];
 
     if($password == $passwordpanel && $login == $loginpanel){
-        $_SESSION['key'] = mt_rand(1,1000);
+        $_SESSION['key'] = password_hash(mt_rand(1,1000), PASSWORD_DEFAULT);
         header('Location: ../panel.php?key=' . $_SESSION['key'] . '');
     } else {
         $_SESSION['message'] = 'Не верный логин или пароль';
