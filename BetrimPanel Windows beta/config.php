c49ecb01ba5e97c63bb8220e0ae511ef0c54699e
Zloger <79506370+haker20SZs@users.noreply.github.com>
2022-10-13 16:12:30 +0700

    Add files via upload

---
+++ BetrimPanel Windows beta/config.php
@@ -0,0 +1,14 @@
+<?php
+
+ $lang = "ru"; #ru/en#
+
+ $log_file = "server.log"; #server.log/server.txt#
+
+ $rconHost = "localhost";
+ $rconPort = "Ваш порт от сервера";
+ $rconPassword = "Ваш пароль ркон";
+ 
+ $loginpanel = "admin";
+ $passwordpanel = "root";
+
+?>
