e9af7044875b4d3b06734cbac5fd1ae4dd2cd667
Zloger <79506370+haker20SZs@users.noreply.github.com>
2022-12-04 01:55:42 +0700

    Add files via upload

---
+++ BetrimPanel Windows beta/script/install-srv.bat
@@ -0,0 +1,15 @@
+@echo off
+
+IF EXIST "home\srv\" (
+    rd home\srv\
+    mkdir "srv";
+    taskkill /F /IM php.exe
+) ELSE (
+    rd home\srv\
+    mkdir "home\srv\";
+    taskkill /F /IM php.exe
+)
+
+echo Loding file....
+copy %cd%\data\server.properties %cd%\home\srv\server.properties
+"%cd%\7z.exe" x "%cd%\data\bin.zip" -o"%cd%\home\srv\bin\" -r -y && "%cd%\7z.exe" x "%cd%\data\src.zip" -o"%cd%\home\srv\src\" -r -y && exit
---
+++ BetrimPanel Windows beta/vendor/signin.php
@@ -0,0 +1,35 @@
+<?php
+    session_start();
+
+    include '../config.php';
+
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+
+    if($lang == "ru"){
+       $text = 'Не верный логин или пароль';
+    }elseif($lang == "en"){
+       $text = 'Wrong login or password';
+    }else{
+        echo "
+        <style>
+            body{
+                background: #111;
+                color: #f4f4f4;
+            }
+        </style>
+        <link rel='shortcut icon' href='./images/favicon.png' type='image/png'>
+        <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+        <title>Извините но данный скрипт поддерживает два языковых пакета</title>
+        <p style='text-align:center'><br>Извините но данный скрипт поддерживает два языковых пакета<br>Установите языковой пакет RU/EN в файле config.php</p>";
+    }
+
+    if($password == $passwordpanel && $login == $loginpanel){
+        $_SESSION['hash'] = password_hash(mt_rand(1,1000), PASSWORD_DEFAULT);
+        header('Location: ../panel.php?hash=' . $_SESSION['hash'] . '');
+    } else {
+        $_SESSION['message'] = $text;
+        header('Location: ../index.php');
+    }
+?>
