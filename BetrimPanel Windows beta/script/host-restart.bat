f0a69d79e1767cd36acc71d1baa7a83b61722ba1
Zloger <79506370+haker20SZs@users.noreply.github.com>
2022-12-11 07:02:08 +0700

    Delete BetrimPanel Windows beta directory

--- BetrimPanel Windows beta/script/host-restart.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd %cd%\script\
-taskkill /F /IM php.exe
-start start.vbs
-exit
--- BetrimPanel Windows beta/script/remove-srv.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rd /S /Q home\srv\
-taskkill /F /IM php.exe
-del /F /Q home\srv\
-taskkill /F /IM cmd.exe
-exit
