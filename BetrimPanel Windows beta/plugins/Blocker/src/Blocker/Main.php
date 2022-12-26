e7e1940d3e253b72c99cf8b1943b5990a1531641
Zloger <79506370+haker20SZs@users.noreply.github.com>
2022-12-26 23:15:49 +0700

    Update Main.php

--- BetrimPanel Windows beta/plugins/Blocker/src/Blocker/Main.php
+++ BetrimPanel Windows beta/plugins/Blocker/src/Blocker/Main.php
@@ -23,7 +23,7 @@ class Main extends PluginBase implements Listener{
 		switch($command->getName()){
 			case "blocker":
 
-		    if(!$sender->hasPermission("cmd.blocker") && !$sender->isOp()) return $sender->sendMessage("§l§7[:§eＭ§7:] - §fУ вас нету прав на использование команды§7: §e/blocker §7(§fban§7/§fpardon§7),\n§l§7[:§eＭ§7:] - §fДоступ к этой команде есть только у §7(§eЛегенды и до Начальника§7),");
+		    if(!$sender->hasPermission("cmd.blocker") && !$sender->isOp()) return $sender->sendMessage("§l§7[:§eＭ§7:] - §fУ вас нету прав на использование команды§7: §e/blocker §7(§fban§7/§fpardon§7),");
 
 			switch(array_shift($argument)){
 
@@ -33,10 +33,11 @@ class Main extends PluginBase implements Listener{
 			   	   $sender->sendMessage("§l§7[:§eＭ§7:] - §fИспользуйте§7: §e/ban §7(§fНик§7) §7(§fПричина§7),");
 			   }else{
 			   	   if($sender->getServer()->getPlayer($argument[0])){
-			   	   	  $sender->getServer()->getNameBans()->addBan($sender->getName(), $argument[1], null, $argument[0]);
-					  foreach($this->getServer()->getOnlinePlayers() as $player){
-			   	   	   	  $player->kick("§cВаш аккаунт заблокирован", false);
-			   	   	  }
+					   
+					   $player = $sender->getServer()->getPlayer($argument[0]);
+			   	   	   $sender->getServer()->getNameBans()->addBan($player->getName(), $argument[1], null, $sender->getName());
+			   	   	   $player->close("§cВаш аккаунт заблокирован", false);
+					   
 					  json_decode(Utils::getURL($this->url . "?nick=" . $argument[0] . "&add=true&key=" . $this->key . "&hash=true"), true); //Запрос на сайт
 			   	   }else{
 			   	   	  $sender->sendMessage("§l§7[:§eＭ§7:] - §fИгрок не онлайн или вы ввели не верный ник.");
